361ee0d325ba9f06b23d3e0e825aa426cd66d2ff
knyazov <tantrumak@gmail.com>
2022-05-09 17:00:57 +0600

    was added java classes and bdmanager

---
+++ src/DBManager/DBManager.java
@@ -0,0 +1,26 @@
+package DBManager;
+
+import Item.Item;
+
+import java.util.ArrayList;
+
+public class DBManager {
+    private static ArrayList<Item> allitems = new ArrayList<Item>();
+    public static Long id = 6L;
+    static {
+        allitems.add(new Item(1L,"MacBook Pro 16", 2500));
+        allitems.add(new Item(2L,"MacBook Pro 13", 1500));
+        allitems.add(new Item(3L,"iPhone 13", 1200));
+        allitems.add(new Item(4L,"iPhone 13 Pro Max", 1800));
+        allitems.add(new Item(5L,"iPhone 12", 1000));
+    }
+    public static void addItem(Item item){
+        item.setId(id);
+        allitems.add(item);
+        id++;
+    }
+
+    public static ArrayList<Item> getAllitems(){
+        return allitems;
+    }
+}
--- src/servlets/HomeServlet.java
+++ src/servlets/HomeServlet.java
@@ -1,5 +1,8 @@
 package servlets;
 
+import DBManager.DBManager;
+import Item.Item;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,17 +10,37 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 @WebServlet(value = "/home")
 public class HomeServlet extends HttpServlet {
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ArrayList<Item> allitems = DBManager.getAllitems();
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Good Luck!</h1>");
+        out.println("<table cellpadding = '20px'>");
+        out.println("<thead>");
+        out.println("<tr><th>ID</th><th>NAME</th><th>PRICE</th></tr>");
+        out.println("</thead>");
+        out.println("<tbody>");
 
+        for (Item it :
+                allitems) {
+            out.println("<tr>");
+            out.println("<td>" + it.getId() + "</td>");
+            out.println("<td>" + it.getName() + "</td>");
+            out.println("<td>" + it.getPrice() + "</td>");
+            out.println("</tr>");
+        }
+
+        out.println("</tbody>");
+        out.println("</table");
     }
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter out = resp.getWriter();
-        out.print("<h1>JAVA EE</h1>");
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
     }
 }
